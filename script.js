edeb22f515a1ffc14b760d3fd2c3b8df660aafbb
Jefferson Otacilio <jeffersonotacilio1@gmail.com>
2023-11-10 10:29:16 -0300

    Code

---
+++ script.js
@@ -0,0 +1,10 @@
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'),{
+    center: {lat: -15.79291306747657, lng: -47.884303763574906},
+    zoom: 12,
+    mapId: '4c3e9867fe5206c'
+
+  });
+}
+
+//-15.79291306747657, -47.884303763574906
